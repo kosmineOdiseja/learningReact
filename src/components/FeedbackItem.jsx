5a0c27c1247d6e008dec772313b28905ae8df15d
kosmineOdiseja <aliusbosas@gmail.com>
2022-01-31 16:47:38 +0200

    Events and prop drilling via onClick and function

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -7,9 +7,17 @@ function FeedbackItem({ item }) {
  // [ destructuring the array here of what the function returns ]
  // [ first value is name of the state, second value is the function to update the state ]
 
+// Secont approach to set value of state 
+ const handleClick = () => {
+	 console.log(item.id)
+ }
   return (
 	  <Card>	
 		<div className="num-display">{item.rating}</div>
+ {/* item.id is the unique id of the feedback item which we can see in the console by clicking the feedback item */}
+		<button onClick={handleClick} className="close">
+			<FaTimes color="purple" />
+		</button>
 		<div className="text-display">{item.text}</div>
 	  </Card>
 	  
